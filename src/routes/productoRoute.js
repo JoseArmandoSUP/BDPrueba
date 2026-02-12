ceb24dd98dd1f109dccf1fbd9be0c308e83c4106
JoseArmandoSUP <124048843@upq.edu.mx>
2026-02-11 20:32:58 -0600

    Practica 2.2

--- src/routes/productoRoute.js
+++ src/routes/productoRoute.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const {poblarProductos} = require('../controllers/externalController');
+const {poblarProductos, buscarCoincidenciasEnElNombre, buscarProductosDeCategoria} = require('../controllers/externalController');
 const router = express.Router();
 
 router.post('/poblar', poblarProductos);
+router.get('/mostrar/:coincidencia', buscarCoincidenciasEnElNombre);
+router.get('/categoria/:cat', buscarProductosDeCategoria)
 
 module.exports = router;
