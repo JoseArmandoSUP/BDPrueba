36c1e3ce1fe7c38ac059752edb64c1ddace74108
JoseArmandoSUP <124048843@upq.edu.mx>
2026-02-21 11:58:28 -0600

    .

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 const pool = require('../config/db');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
     const {email, password} = req.body;
@@ -14,7 +15,7 @@ const register = async (req, res) => {
         const newUser = await pool.query('INSERT INTO usuarios (email, password) VALUES ($1, $2)', [email, passwordHash]);
 
         res.status(201).json({
-            mag: "Usuario registrado con exito",
+            msg: "Usuario registrado con exito",
             user: newUser.rows[0]
         });
     }catch (error){
@@ -23,4 +24,42 @@ const register = async (req, res) => {
     }
 };
 
-module.exports = {register}
+const login = async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    try{
+        const result = await pool.query('SELECT * FROM usuarios WHERE email = $1 AND password = $2', [email, password]);
+        if(result.rows.length === 0){
+            return res.status(400).json({ mensaje: "Credenciales Invalidas" });
+        }
+
+        const usuario = result.rows[0];
+        const isMatch = await bcrypt.compare(password, usuario.password);
+        
+        if(!isMatch){
+            return res.status(400).json({ mensaje: "Credenciales Invalidas (Password)" }); 
+        }
+
+        const playload = {
+            id: usuario.id,
+            rol: usuario.rol,
+            email: usuario.email
+        };
+
+        const token = {
+            playload, 
+            process.env.JWT_SECRET, 
+            { expiresIn: '1h' }
+        };
+
+        res.json({
+            msg: "Bienvenido",
+            token: token
+        });
+    }catch (error){
+        console.error(error);
+        res.status(500).json({ error: 'Error en el servidor' });
+    }
+};
+
+module.exports = {register, login}
--- src/routes/authRaute.js
+++ src/routes/authRaute.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const authControllee = require('../controllers/authController');
+const authController = require('../controllers/authController');
 
-router.post('/register', authControllee.register);
-// router.post('/login', authContorllee.login);
+router.post('/register', authController.register);
+router.post('/login', authController.login);
 
 module.exports = router;
